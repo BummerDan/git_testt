fa72280753a5908b6caf83b215c7d4abb0c4b97a
BummerDan <110871873+BummerDan@users.noreply.github.com>
2023-12-22 12:47:06 -0700

    updating random notes in .py propgram

---
+++ pcc_notebook.py
@@ -0,0 +1,98 @@
+# # magicians = ['alice', 'david', 'carolina']
+# # for magician in magicians:
+# #     print(f"{magician.title()}, that was a great trick!")
+# #     print(f"I cant wait to see what you have for us next, {magician.title()}.\n")
+# # print("Thanks everyone. That was a good show!")
+
+# fav_pizzas = ['cheese', 'hawaian', 'sausage and onion']
+# #print(fav_pizzas)
+# for pizza in fav_pizzas:
+#     print(f"I love to eat {pizza} pizza!")
+# print(f"I just really like pizza")
+# for value in range(1, 5):
+#     print(value)
+# numbers = list(range(1,6))
+# print(numbers)
+
+# twenty = []
+# for value in range(1,21):
+#     twenty.append(value)
+# print(twenty)
+
+#heres another way to make a list
+# twenty_numbers = list(range(1, 21))
+# print(twenty_numbers)
+# #heres how you can use a step to increment range in a given way:
+# # even_numbers = list(range(2, 11, 2))
+# #make a list of numbers from 1-20
+# # twenty = list(range(1,21))
+# # print(twenty)
+# Start a for loop, printing every number in the list
+# for number in twenty:
+    # print(number)
+    
+# million = list(range(1,1_000_001))'audi', 'bmw', 'subaru', 'toyota'
+# for number in odd_numbers:
+#     print(number)
+
+# threes = list(range(3,33,3))
+# for three in threes:
+    # print(three)
+ 
+#This creates a list from empty list using range
+  # cubes = []
+# for value in range(1,11):
+#     cubed = value ** 3
+#     cubes.append(cubed)
+#     print(cubed)
+#
+# This is going to accomplish the same thing but using list comprehension
+# cubes = [number**3 for number in range(1,11)]
+# for number in cubes:
+#     print(number)
+# 
+#players = ['charles','martina','michael','florence','elli']
+# print("Here are the first three players on my team:")
+# for player in players[:3]:
+#     print(player.title())
+
+#this is how you copy a list, and how you can edit/append them'audi', 'bmw', 'subaru', 'toyota'
+# print(my_foods)
+# print("\nMy friend's favorites foods are:")
+# print(friends_foods)
+# my_foods.append('cannoli')
+# friends_foods.append('ice cream')
+# print("\nMy new favorites are:")
+# print(my_foods)
+# print("\nMy freinds new favoritess are:")
+# print(friends_foods)
+
+#This is some tuple shit
+# dimensions = (200, 50)
+# print("Origninal Dimensions:")
+# for dimension in dimensions:
+#     print(dimension)
+
+# dimensions = (400,500)
+# print("\nNew Dimensions:")
+# for dimension in dimensions:
+#     print(dimension)
+# buffet = ('Burgers','Cum', 'Sushi', 'Nuggets', 'Steak')
+# print("This was the menu before the change:")'audi', 'bmw', 'subaru', 'toyota'
+
+#This is the maximum some nerds said you should use per line (89 char) xxxxxxxxxxxxxxxxx
+#simple if example
+
+
+# #THis is a simple example of an if statement
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+
+age_0= 22
+age_1 = 18
+age_0 >=21 and age_1 >=21
